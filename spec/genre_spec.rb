2c64c0c56571feec52b9f833880e02dd077ec96f
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-19 17:16:25 +0100

    Write the unit test for the initialization of the genre class

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,4 +1,29 @@
 require_relative '../classes/genre'
 
 describe Genre do
+  before(:each) do
+    @genre = Genre.new(
+      name: 'Action',
+      items: []
+    )
+  end
+
+  context 'Testing the Genre class' do
+    it 'The initialize method should create a item with 3 variables' do
+      expect(@genre.instance_variables.length).to eq 3
+      expect(@genre.instance_variables).to eq %i[@id @name @items]
+    end
+
+    it 'The initialize method should create an instance variable of name' do
+      expect(@genre.instance_variable_get(:@name)).to eq 'Action'
+    end
+
+    it 'The initialize method should create an instance variable of items' do
+      expect(@genre.instance_variable_get(:@items)).to eq []
+    end
+
+    it 'The initialize method should create an instance variable of id' do
+      expect(@genre.instance_variable_get(:@id)).to be_a String
+    end
+  end
 end
