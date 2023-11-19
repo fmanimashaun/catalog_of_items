e9ac898b67e1ca0534c395b7b7cc86081bb1a0d1
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-19 17:10:08 +0100

    Write the unit test for the initialization of the source class

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,4 +1,29 @@
 require_relative '../classes/source'
 
 describe Source do
+  before(:each) do
+    @source = Source.new(
+      name: 'New York Times',
+      items: []
+    )
+  end
+
+  context 'Testing the Source class' do
+    it 'The initialize method should create a item with 3 variables' do
+      expect(@source.instance_variables.length).to eq 3
+      expect(@source.instance_variables).to eq %i[@id @name @items]
+    end
+
+    it 'The initialize method should create an instance variable of name' do
+      expect(@source.instance_variable_get(:@name)).to eq 'New York Times'
+    end
+
+    it 'The initialize method should create an instance variable of items' do
+      expect(@source.instance_variable_get(:@items)).to eq []
+    end
+
+    it 'The initialize method should create an instance variable of id' do
+      expect(@source.instance_variable_get(:@id)).to be_a String
+    end
+  end
 end
