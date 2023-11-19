5ef330b181dcef155597dfbae5b666002adda55a
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-19 21:05:00 +0100

    Write the unit test for the add_item of the genre class

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,10 +1,15 @@
 require_relative '../classes/genre'
+require_relative '../classes/item'
 
 describe Genre do
   before(:each) do
     @genre = Genre.new(
       name: 'Action',
       items: []
+    )
+
+    @item = Item.new(
+      publish_date: '1967-01-01'
     )
   end
 
@@ -24,6 +29,17 @@ describe Genre do
 
     it 'The initialize method should create an instance variable of id' do
       expect(@genre.instance_variable_get(:@id)).to be_a String
+    end
+  end
+
+  context '#add_item' do
+    it 'The add_item method should add a item to the items array' do
+      @genre.add_item(@item)
+      expect(@genre.instance_variable_get(:@items).length).to eq 1
+      expect(@genre.instance_variable_get(:@items)).to eq [@item]
+      expect(@genre.instance_variable_get(:@items).first).to be_a Item
+      expect(@item.instance_variable_get(:@genre)).to be_a Genre
+      expect(@item.genre).to eq(@genre)
     end
   end
 end
