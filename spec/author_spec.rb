6eab6c7cfa01631459cf594d1bb30ba76e072c32
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-19 17:03:44 +0100

    Write the uniti test for the initialization of the author class

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,4 +1,30 @@
 require_relative '../classes/author'
 
 describe Author do
+  before(:each) do
+    @author = Author.new(
+      name: 'J. K. Rowling',
+      items: []
+    )
+  end
+
+  context 'Testing the Auhtor class' do
+    it 'The initialize method should create a item with 3 variables' do
+      expect(@item.instance_variables.length).to eq 3
+      expect(@item.instance_variables).to eq %i[@id @name @items]
+      # expect(@item.instance_variable_get(:@id)).to be_a String
+    end
+
+    it 'The initialize method should create an instance variable of name' do
+      expect(@item.instance_variable_get(:@name)).to eq 'J. K. Rowling'
+    end
+
+    it 'The initialize method should create an instance variable of items' do
+      expect(@item.instance_variable_get(:@items)).to eq []
+    end
+
+    it 'The initialize method should create an instance variable of id' do
+      expect(@item.instance_variable_get(:@id)).to be_a String
+    end
+  end
 end
