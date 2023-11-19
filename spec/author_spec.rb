c5489c17c08ccdeafb03534e81b39c1ef7892881
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-19 17:09:47 +0100

    Update the author unit test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -8,22 +8,22 @@ describe Author do
     )
   end
 
-  context 'Testing the Auhtor class' do
+  context 'Testing the Author class' do
     it 'The initialize method should create a item with 3 variables' do
-      expect(@item.instance_variables.length).to eq 3
-      expect(@item.instance_variables).to eq %i[@id @name @items]
+      expect(@author.instance_variables.length).to eq 3
+      expect(@author.instance_variables).to eq %i[@id @name @items]
     end
 
     it 'The initialize method should create an instance variable of name' do
-      expect(@item.instance_variable_get(:@name)).to eq 'J. K. Rowling'
+      expect(@author.instance_variable_get(:@name)).to eq 'J. K. Rowling'
     end
 
     it 'The initialize method should create an instance variable of items' do
-      expect(@item.instance_variable_get(:@items)).to eq []
+      expect(@author.instance_variable_get(:@items)).to eq []
     end
 
     it 'The initialize method should create an instance variable of id' do
-      expect(@item.instance_variable_get(:@id)).to be_a String
+      expect(@author.instance_variable_get(:@id)).to be_a String
     end
   end
 end
